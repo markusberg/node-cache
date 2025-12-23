9ad814e52f8727311e530990d9bcef23eb17a558
Markus Berg <markus@kelvin.nu>
2025-12-23 13:39:27 +0100

    refactor: move to esmodules

--- src/helpers.js
+++ src/helpers.js
@@ -1,5 +1,4 @@
-(function() {
-  /*
+/*
   Generates a random string of given length
 
   @param {Number} length - length of the returned string
@@ -7,35 +6,35 @@
 
   @return {String} generated random string
   */
-  exports.randomString = function(length, withnumbers = true) {
-    var chars, i, randomstring, rnum, string_length;
-    chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-    if (withnumbers) {
-      chars += "0123456789";
-    }
-    string_length = length || 5;
-    randomstring = "";
-    i = 0;
-    while (i < string_length) {
-      rnum = Math.floor(Math.random() * chars.length);
-      randomstring += chars.substring(rnum, rnum + 1);
-      i++;
-    }
-    return randomstring;
-  };
+export function randomString(length, withnumbers = true) {
+  var chars, i, randomstring, rnum, string_length
+  chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+  if (withnumbers) {
+    chars += '0123456789'
+  }
+  string_length = length || 5
+  randomstring = ''
+  i = 0
+  while (i < string_length) {
+    rnum = Math.floor(Math.random() * chars.length)
+    randomstring += chars.substring(rnum, rnum + 1)
+    i++
+  }
+  return randomstring
+}
 
-  /*
+/*
   Generates a random number between 0 and `max`
 
   @param {Number} max
 
   @return {Number} generated random number
   */
-  exports.randomNumber = function(max) {
-    return Math.floor(Math.random() * (max + 1));
-  };
+export function randomNumber(max) {
+  return Math.floor(Math.random() * (max + 1))
+}
 
-  /*
+/*
   Subtracts all objB keys from objA keys and returns the result.
   Both objects should have identical keys with numeric values
 
@@ -44,15 +43,13 @@
 
   @return {Object} Object with the diffed values
   */
-  exports.diffKeys = function(objA, objB) {
-    var diff, key;
-    diff = {};
-    for (key in objA) {
-      if (objB.hasOwnProperty(key)) {
-        diff[key] = objA[key] - objB[key];
-      }
+export function diffKeys(objA, objB) {
+  var diff, key
+  diff = {}
+  for (key in objA) {
+    if (objB.hasOwnProperty(key)) {
+      diff[key] = objA[key] - objB[key]
     }
-    return diff;
-  };
-
-}).call(this);
+  }
+  return diff
+}
